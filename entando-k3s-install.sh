65615d1145134458fd046d2f674014d054fa0039
Sergio Molino <smolino@users.noreply.github.com>
2022-07-13 15:25:02 +0200

    Update entando-k3s-install.sh

--- entando-k3s-install.sh
+++ entando-k3s-install.sh
@@ -24,7 +24,7 @@ echo "Creating Namespace $namespace"
 echo ""
 echo "##################################################################################"
 echo "##################################################################################"
-sudo kubectl create namespace $namespace
+kubectl create namespace $namespace
 
 
 echo ""
@@ -45,7 +45,7 @@ echo ""
 echo "##################################################################################"
 echo "##################################################################################"
 
-sudo kubectl apply -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.0.0/manifests/k8s-116-and-later/namespace-scoped-deployment/cluster-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.0.0/manifests/k8s-116-and-later/namespace-scoped-deployment/cluster-resources.yaml
 
 echo "##################################################################################"
 echo "##################################################################################"
@@ -55,7 +55,7 @@ echo ""
 echo "##################################################################################"
 echo "##################################################################################"
 
-sudo kubectl apply -n $namespace -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.0.0/manifests/k8s-116-and-later/namespace-scoped-deployment/namespace-resources.yaml
+kubectl apply -n $namespace -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.0.0/manifests/k8s-116-and-later/namespace-scoped-deployment/namespace-resources.yaml
 
 echo "##################################################################################"
 echo "##################################################################################"
@@ -65,7 +65,7 @@ echo ""
 echo "##################################################################################"
 echo "##################################################################################"
 sleep 10
-sudo kubectl get svc -n ingress-nginx | grep LoadBalancer | awk '{print $4}' | while read HOST;do
+kubectl get svc -A | grep LoadBalancer | awk '{print $4}' | while read HOST;do
 echo -e "
 apiVersion: entando.org/v1
 kind: EntandoApp
