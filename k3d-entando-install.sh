6b8b79b9327468b64f1eb168ca2a71c2895c12b0
Sergio Molino <smolino@users.noreply.github.com>
2022-07-18 16:59:02 +0200

    Update k3d-entando-install.sh

--- k3d-entando-install.sh
+++ k3d-entando-install.sh
@@ -2,5 +2,6 @@
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d cluster create entando-quickstart --image="rancher/k3s:v1.21.9-k3s1"
 kubectl cluster-info
-sleep 120
+Deploying kubernetes cluster - please wait 1 minute
+sleep 60
 curl -sfL https://raw.githubusercontent.com/entando/oc_installation_script/master/entando-k3s-install.sh > installation_script.sh && chmod +x installation_script.sh && ./installation_script.sh entando quickstart
