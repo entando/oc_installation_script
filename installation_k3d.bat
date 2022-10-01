5493a8e62cf09a2c43c2fbf2c0a87f7dfed31842
Sergio Molino <smolino@users.noreply.github.com>
2022-10-01 18:20:56 +0200

    Add files via upload

---
+++ installation_k3d.bat
@@ -0,0 +1,13 @@
+choco install k3d -y
+choco install jq -y
+choco install yq -y
+choco install kubernetes-helm -y
+# Create user profile file if it doesn't exist
+if ( -not ( Test-Path $Profile ) ) { New-Item -Path $Profile -Type File -Force }
+# Append the k3d completion to the end of the user profile
+k3d completion powershell | Out-File -Append $Profile
+k3d cluster create entandotest --image="rancher/k3s:v1.21.9-k3s1"
+kubectl cluster-info
+kubectl get nodes
+curl -sfL https://raw.githubusercontent.com/entando/oc_installation_script/master/entando-k3s-install.sh > installation_script.sh
+./installation_script.sh entando quickstart
