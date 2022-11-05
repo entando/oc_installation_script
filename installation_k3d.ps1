fa3c03de8eb39b30197ea25bbef150f759102a34
Sergio Molino <smolino@users.noreply.github.com>
2022-11-05 09:01:44 +0100

    Update installation_k3d.ps1

--- installation_k3d.ps1
+++ installation_k3d.ps1
@@ -9,5 +9,38 @@ k3d completion powershell | Out-File -Append $Profile
 k3d cluster create entandotest --image="rancher/k3s:v1.21.9-k3s1"
 kubectl cluster-info
 kubectl get nodes
-curl -s https://raw.githubusercontent.com/entando/oc_installation_script/master/entando-k3s-install.sh > installation_script.sh
-./installation_script.sh entando quickstart
+kubectl create namespace entando
+kubectl apply -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.1.1/manifests/k8s-116-and-later/namespace-scoped-deployment/cluster-resources.yaml
+echo "##################################################################################"
+echo "##################################################################################"
+echo ""
+echo "Creating Namespace Resources"
+echo ""
+echo "##################################################################################"
+echo "##################################################################################"
+
+kubectl apply -n entando -f https://raw.githubusercontent.com/entando-k8s/entando-k8s-operator-bundle/v7.1.1/manifests/k8s-116-and-later/namespace-scoped-deployment/namespace-resources.yaml
+
+sleep 10
+kubectl get svc -A | grep LoadBalancer | awk '{print $5}' | while read HOST;do
+echo -e "
+apiVersion: entando.org/v1
+kind: EntandoApp
+metadata:
+  namespace: entando
+  name: quickstart
+spec:
+  environmentVariables: []
+  dbms: embedded
+  ingressHostName: $HOST.nip.io
+  standardServerImage: eap
+  replicas: 1" | kubectl apply -f -; done
+  echo ""
+echo "##################################################################################"
+echo "##################################################################################"
+echo ""
+echo "Namespace $namespace is created and $appname application is deploying"
+echo "Wait around 10 minutes, when application is deployed it is available at:"
+echo ""
+kubectl get svc -A | grep LoadBalancer | awk '{print $5}' |while read HOST;do
+echo "http://$HOST.nip.io/app-builder/";done
